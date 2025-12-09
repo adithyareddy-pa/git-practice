dc5d8ab4b3f65be990fa7c2773ff0ac754a47f87
adithyareddy-pa <p.a.adithyareddy@gmail.com>
2025-12-10 00:36:39 +0530

    i am writing conv file

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,5 +3,5 @@ PERSON1=Adithya
 PERSON2=Ram
 
 echo "Hi $PERSON1, How are you?"
-echo "Hi {$PERSON2}, I am fine. How are you?"
+echo "Hi ${PERSON2}, I am fine. How are you?"
 echo "Fine $PERSON1, how is Baahubali movie?"
