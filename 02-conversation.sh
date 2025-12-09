6f376fab4638368f7541e9e44b6c8303ee705909
adithyareddy-pa <p.a.adithyareddy@gmail.com>
2025-12-10 00:52:27 +0530

    i am writing conv

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,4 +5,5 @@ PERSON2=Ram
 echo "Hi $PERSON1, How are you?"
 echo "Hi ${PERSON2}, I am fine. How are you?"
 echo "Fine $PERSON1, how is Baahubali movie?"
+echo "Coming out from theatre"
 echo "Excellent movie ${PERSON2}, Superb Direction by Rajamouli !!"
